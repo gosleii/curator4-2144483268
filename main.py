76f2eb740ff3af06e07ae830f1250858ce131d45
gosleii <sleii@inbox.ru>
2024-08-08 17:26:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import telebot
+
+bot = telebot.TeleBot("7192629699:AAEFE1nYvkoCTPKLcICN4PQd-tFW02Iup5Q")
+
+@bot.message_handler(commands=["start"])
+def your_handler(message):
+    bot.send_message(message.chat.id, "Салам")
+
+@bot.message_handler(commands=["school_1"])
+def your_handler(message):
+    bot.send_message(message.chat.id, "*ссылка: https://beeschool.team*", parse_mod="Markdown")
+
+@bot.message_handler(commands=["school_2"])
+def your_handler(message):
+    bot.send_message(message.chat.id, "*ссылка: https://100points.ru/student/courses*", parse_mod="Markdown")
+
+@bot.message_handler(commands=["school_3"])
+def your_handler(message):
+    bot.send_message(message.chat.id, "*ссылка: https://edu.sirius.online/#/*", parse_mod="Markdown")
+
+bot.infinity_polling()
